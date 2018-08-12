17b8e186aa67ddef2ca97a88492db2f5d34d8e02
Scott Hall <scotthall127@gmail.com>
2018-08-12 18:12:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  attr_accessor :name
+
+  @@all = []
+  @@names = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @@names << name unless @@names.include?(name)
+  end
+
+  def self.all
+    puts @@names
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
